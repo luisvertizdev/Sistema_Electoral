c5628df90289df54c26a82b58ba010f81b069660
Luis Vértiz <luisvertiz@MacBook-Pro-de-Luis.local>
2026-06-25 17:01:42 -0500

    add menus

--- main.cpp
+++ main.cpp
@@ -6,33 +6,92 @@ using namespace std;
 
 int main() {
 
-    int opcion1 = 0;
-    int opcion1_1 = 0;
-    string dni = 0;
+    int opcionMenuPrincipal = 0;
+    int opcionMenuSecundario = 0;
+    int opcionMenuConteos = 0;
+    string dni = "";
+    int opcionCandidato = 0;
+    string candidatos[3] = { "Roberto Sanchez", "Keiko Fujimori", "Rafael López Aliaga"};
+    int contadorVotosCandidato[3] = {0, 0, 0};
 
     // MENU 1
+
     do {
         cout << "***** MENU PRINCIPAL *****" << endl;
-        cout << "1. IDENTIFICACIÓN" << endl;
-        cout << "2. SALIR" << endl;
-        cout << "Ingrese una opción: ";
-        cin >> opcion1;
+        cout << "1. REGISTRO DE VOTO" << endl;
+        cout << "2. CONTEO DE VOTOS" << endl;
+        cout << "3. SALIR " << endl;
+        cout << "Ingresa la opción: ";
+        cin >> opcionMenuPrincipal;
 
-        switch (opcion1) {
+        switch (opcionMenuPrincipal) {
             case 1: {
                 do {
-                    cout << "***** DATOS PERSONALES *****" << endl;
-                    cout << "Ingresa tu DNI: ";
-                    cin >> dni;
-                } while (dni.length() != 8);
+                    cout << "***** MENU SECUNDARIO *****" << endl;
+                    cout << "1. IDENTIFICACIÓN" << endl;
+                    cout << "2. VOLVER" << endl;
+                    cout << "Ingrese una opción: ";
+                    cin >> opcionMenuSecundario;
+
+                    switch (opcionMenuSecundario) {
+                        case 1: {
+                            do {
+                                cout << "***** DATOS PERSONALES *****" << endl;
+                                cout << "Ingresa tu DNI: ";
+                                cin >> dni;
+                            } while (dni.length() != 8);
+
+                            do {
+                                cout << "***** LISTA DE CANDIDATOS *****" << endl;
+
+                                for (int i = 0; i < 3; i++) {
+                                    cout << i+1 << ". " << candidatos[i] << endl;
+                                }
+                                cout << "Elige la opción de tu candidato: ";
+                                cin >> opcionCandidato;
+                            } while (opcionCandidato < 1 || opcionCandidato > 3);
+
+                            cout << "Has votado por " << candidatos[opcionCandidato - 1] << "." << endl;
+                            contadorVotosCandidato[opcionCandidato - 1] = contadorVotosCandidato[opcionCandidato - 1] + 1;
+
+
+                        }
+                        case 2: {
+                            cout << "El sistema ha finalizado" << endl;
+                            return 0;
+                        }
+                        default: {}
+                    }
+                } while (opcionMenuSecundario < 1 || opcionMenuSecundario > 2);
             }
+
             case 2: {
-                cout << "El sistema ha finalizado" << endl;
-                return 0;
+                do {
+                    cout << "***** MENU SECUNDARIO *****" << endl;
+                    cout << "1. CANTIDAD DE VOTOS TOTALES" << endl;
+                    cout << "2. CANTIDAD DE VOTOS POR PROVINCIA" << endl;
+                    cout << "3. VOLVER " << endl;
+                    cout << "Ingresa la opción: ";
+                    cin >> opcionMenuConteos;
+                } while (opcionMenuConteos < 1 || opcionMenuConteos > 3);
+
+                switch (opcionMenuConteos) {
+                    case 1: {
+                        cout << "***** VOTOS TOTALES *****" << endl;
+                        for (int i = 0; i < 3; i++) {
+                            cout << i+1 << ". " << candidatos[i] << " " << contadorVotosCandidato[opcionCandidato - 1] << " votos" << endl;
+                        }
+                        break;
+                    }
+                    case 2: {
+                        cout << "***** VOTOS POR PROVINCIA *****" << endl;
+                    }
+                }
             }
-            default: {}
         }
-    } while (opcion1 < 1 || opcion1 > 2);
+    } while (opcionMenuPrincipal < 1 || opcionMenuPrincipal > 3);
+
+
 
     return 0;
     // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
